18eedd7407b08ecf501514e1761d6ccd6c54752b
Thomas P <132946099+Tomjp424@users.noreply.github.com>
2024-08-10 20:39:40 -0400

    optimized javascript and added functions

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,9 +1,10 @@
 const userData = {
     isExotic: undefined,
-    isFurry: undefined,
     isFriendly: undefined,
+    isFurry: undefined,
     isLarge: undefined,
 }
+const userDataOrder = ["isExotic", "isFriendly", "isFurry", "isLarge"];
 const animalArray = [
     {name: "Capybara", image: "#", isExotic: true, isFriendly: true, isFurry: true, isLarge: true},
     {name: "Lemur", image: "#", isExotic: true, isFriendly: true, isFurry: true, isLarge: false},
@@ -22,12 +23,14 @@ const animalArray = [
     {name: "Alligator", image: "#", isExotic: false, isFriendly: false, isFurry: false, isLarge: true},
     {name: "Scorpion", image: "#", isExotic: false, isFriendly: false, isFurry: false, isLarge: false},
 ] 
-const questionsandAnswers = [
+const questionsAndAnswers = [
     {question: "If you're planning a vacation, where would you rather go?", answer1: "Somewhere far away you've never been to before.", answer2: "Somewhere close to home that is familiar and cozy."},
-    {question: "You win a chance to spend a day with a celebrity. Between these two, who's your pick?", answer1: "Jason Momoa", answer2: `Dwayne "The Rock" Johnson`},
     {question: "It's your cheat day. Which would you rather indulge in?", answer1: "Ice Cream", answer2: "Spicy Wings"},
+    {question: "You win a chance to spend a day with a celebrity. Between these two, who's your pick?", answer1: "Jason Momoa", answer2: `Dwayne "The Rock" Johnson`},
     {question: "Time to do some grocery shopping. Where are you buying from?", answer1: "Costco", answer2: "Local Market/General Store"},
 ]
+let questionIndex = -1;
+let matchingAnimal = "";
 
 // code for button display
 let answerButtonOne = document.getElementById('answerButtonOne');
@@ -35,18 +38,15 @@ let answerButtonTwo = document.getElementById('answerButtonTwo');
 let questionText = document.getElementById('questionText');
 
 function showAnswerButtonTwo() {
+    if (questionIndex === -1)
     answerButtonTwo.style.display = 'flex';
 }
 
-answerButtonOne.addEventListener('click', showAnswerButtonTwo);
-
-let questionIndex = 0;
-
 // code for displaying questions
+
 function displayNextQuestion() {
-    if (questionIndex < questionsandAnswers.length) {
-        questionText.textContent = questionsandAnswers[questionIndex].question;
-        questionIndex++;
+    if (questionIndex < questionsAndAnswers.length) {
+        questionText.textContent = questionsAndAnswers[questionIndex].question;
     } else {
         // End of questions
         questionText.textContent = "Congratulations!";
@@ -55,22 +55,58 @@ function displayNextQuestion() {
     }
 }
 
-answerButtonOne.addEventListener('click', displayNextQuestion);
-answerButtonTwo.addEventListener('click', displayNextQuestion);
-
 // code for updating answer buttons
 
-let answer1Index = 0;
-let answer2Index = 0;
-
 function updateAnswerButtons() {
-    if (answer1Index < questionsandAnswers.length && answer2Index < questionsandAnswers.length) {
-        answerButtonOne.textContent = questionsandAnswers[answer1Index].answer1;
-        answerButtonTwo.textContent = questionsandAnswers[answer2Index].answer2;
-        answer1Index++;
-        answer2Index++;
+    if (questionIndex < questionsAndAnswers.length) {
+        answerButtonOne.textContent = questionsAndAnswers[questionIndex].answer1;
+        answerButtonTwo.textContent = questionsAndAnswers[questionIndex].answer2;
     }
 }
 
-answerButtonOne.addEventListener('click', updateAnswerButtons);
-answerButtonTwo.addEventListener('click', updateAnswerButtons);
+// Answer buttons eventListeners
+
+answerButtonOne.addEventListener('click', function() {
+    showAnswerButtonTwo();
+    answerOneValue();
+    questionIndex++;
+    displayNextQuestion();
+    updateAnswerButtons();
+    findMatchingAnimal();
+});
+answerButtonTwo.addEventListener('click', function() {
+    answerTwoValue();
+    questionIndex++;
+    displayNextQuestion();
+    updateAnswerButtons();
+    findMatchingAnimal();
+});
+
+// Code for applying values to userData on answer given
+
+function answerOneValue() {
+    if (questionIndex >= 0) {
+        userData[userDataOrder[questionIndex]] = true;
+    }
+}
+function answerTwoValue() {
+    if (questionIndex >= 0) {
+        userData[userDataOrder[questionIndex]] = false;
+    }
+}
+
+// Evaluating the matching animal
+
+function findMatchingAnimal() {
+    if (questionIndex === questionsAndAnswers.length) {
+        for (let x = 0; x < animalArray.length; x++) {
+            if (userData.isExotic === animalArray[x].isExotic && userData.isFriendly === animalArray[x].isFriendly && userData.isFurry === animalArray[x].isFurry && userData.isLarge === animalArray[x].isLarge) {
+                matchingAnimal = animalArray[x].name;
+                console.log(matchingAnimal);
+                console.log(userData);
+                console.log(animalArray[x]);
+                break
+            }
+        }
+    }
+}
